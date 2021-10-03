c88ec4ee62420e15650f90efa02f0ef46dcbefb3
Hrant Grishyan <83180608+Hades3110@users.noreply.github.com>
2021-10-03 18:40:14 +0400

    add method onChangeSalary

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,7 +2,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item"
 import './employees-list.css'
 
 
-const EmployeesList = ({data , onDelete , onToggleProp}) => {
+const EmployeesList = ({data , onDelete , onToggleProp , onChangeSalary}) => {
     
     const element = data.map(el => {
         const {id , ...itemProps} = el
@@ -10,7 +10,8 @@ const EmployeesList = ({data , onDelete , onToggleProp}) => {
             <EmployeesListItem key={id} 
                 {...itemProps} 
                 onDelete={() => onDelete(id)}
-                onToggleProp={(e) => onToggleProp(id,e.currentTarget.getAttribute('data-toggle'))}/>
+                onToggleProp={(e) => onToggleProp(id,e.currentTarget.getAttribute('data-toggle'))}
+                onChangeSalary={(e) => onChangeSalary(id , e.target.value)}/>
         )
     })
     
