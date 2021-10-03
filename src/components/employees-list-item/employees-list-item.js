c6e0318f3263e1b3a8b47f57784eacea3f34689e
Hrant Grishyan <83180608+Hades3110@users.noreply.github.com>
2021-10-03 18:41:18 +0400

    Update employees-list-item.js

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -3,7 +3,7 @@ import "./employees-list-item.css"
 
 const EmployeesAddForm =(props) => {
      
-    const {name, salary, onDelete , onToggleProp, increase , rise } = props;
+    const {name, salary, onDelete , onToggleProp, increase , rise, onChangeSalary } = props;
     let classNames = "list-group-item d-flex justify-content-between";
     if(increase){
         classNames+=" increase";
@@ -15,7 +15,7 @@ const EmployeesAddForm =(props) => {
     return(
         <li className={classNames} >
             <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+            <input type="text" className="list-group-item-input" defaultValue={salary + "$"} onChange={onChangeSalary}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm "
