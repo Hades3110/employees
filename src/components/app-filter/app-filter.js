f53a6df38b8b366d16ea063fbaa3113dc8282321
Hrant Grishyan <grishyan.hrant@gmail.com>
2021-10-03 15:40:59 +0400

    1.0

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,47 @@
+import './app-filter.css'
+
+
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name:"all", lable:"Все сотрудники"},
+        {name:"rise", lable:"На повышеник"},
+        {name:"moreThen1000", lable:"З/П больше 1000$"}
+    ]
+
+    const buttons = buttonsData.map(({name, lable}) => {
+        const active = props.filter === name;
+        const clazz = active ? "btn-light": "btn-outline-light";
+        return(
+            <button
+                type="button" 
+                className={`btn ${clazz}`}
+                key={name}
+                onClick={() => props.onFilterSelect(name)}>
+                    {lable}
+            </button>
+        )
+    })
+
+    return(
+         <div className="btn-group">
+            {buttons}
+            {/* <button
+                type="button" 
+                className="btn btn-light">
+                    Все сотрудники
+            </button>
+            <button
+                type="button" 
+                className="btn btn-outline-light">
+                    На повышеник
+            </button>
+            <button
+                type="button" 
+                className="btn btn-outline-light">
+                    З/П больше 1000$
+            </button> */}
+         </div>
+    )
+}
+
+export default AppFilter;
