add2df32376e35e6c19210af4243b6783a37cade
Hrant Grishyan <83180608+Hades3110@users.noreply.github.com>
2021-10-05 11:36:58 +0400

    Update app-info.js

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -4,9 +4,12 @@ import './app-info.css'
 const AppInfo = ({employees , increased}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании Hades corp.</h1>
-            <h2>Общее число сотрудников: {employees}</h2>
-            <h2>Премию получат: {increased}</h2>
+            <img src='/img/hades.png' width="400px"></img>
+            <div>
+                <h2>Учет сотрудников в компании</h2>
+                <h4>Общее число сотрудников: {employees}</h4>
+                <h4>Премию получат: {increased}</h4>
+            </div>
         </div>
     )
 }
